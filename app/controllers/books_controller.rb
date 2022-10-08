0ae49f08078bc9b89d8c1e0f23b4cf451c9995d5
EC2 Default User <ec2-user@ip-172-31-6-20.ap-northeast-1.compute.internal>
2022-10-08 14:48:43 +0000

    [Add] 一覧画面追記、ストロングパラメータ設定

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -4,11 +4,16 @@ class BooksController < ApplicationController
   end
 
   def create
-    @book = Book.new
+  end
 
   def show
   end
 
   def edit
+  end
+
+  private
+  def list_params
+    params.require(:book).permit(:title, :body)
   end
 end
